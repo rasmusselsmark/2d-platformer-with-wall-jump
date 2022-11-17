3ca29a613b9120118f166cb94ef8bf96eb01bdea
Rasmus Selsmark <rasmus@unity3d.com>
2022-11-17 09:27:30 +0100

    Added comments to Player controller script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,10 +18,13 @@ public class Player : MonoBehaviour
         Left,
         Right,
     }
+
+    // variable to keep track of which side we last jumped
     LastWallJumpSide lastWallJumpSide = LastWallJumpSide.None;
 
     void Start()
     {
+        // for performance, keep references to various components
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
@@ -32,42 +35,49 @@ public class Player : MonoBehaviour
         if (isDead)
             return;
 
-        float dir = Input.GetAxisRaw("Horizontal");
+        float direction = Input.GetAxisRaw("Horizontal");
 
-        if (dir < 0 && !GroundCheck(Vector2.left, 0.05f))
+        // keep 5 cm distance to make sure we cannot "stick" to walls
+        if (direction < 0 && !GroundCheck(Vector2.left, 0.05f))
         {
-            transform.Translate(dir * Speed * Time.deltaTime, 0, 0);
+            transform.Translate(direction * Speed * Time.deltaTime, 0, 0);
             sr.flipX = true;
         }
-        else if (dir > 0 && !GroundCheck(Vector2.right, 0.05f))
+        else if (direction > 0 && !GroundCheck(Vector2.right, 0.05f))
         {
-            transform.Translate(dir * Speed * Time.deltaTime, 0, 0);
+            transform.Translate(direction * Speed * Time.deltaTime, 0, 0);
             sr.flipX = false;
         }
 
         bool jump = Input.GetButtonDown("Jump");
+
+        // if just jumping from ground, to disable jumping in mid-air)
         if (jump && GroundCheck(Vector2.down))
         {
             rb.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
             lastWallJumpSide = LastWallJumpSide.None;
-        } 
+        }
+
+        // if we have a wall on left side, check that our last walljump wasn't on same side
         else if (jump && GroundCheck(Vector2.left) && lastWallJumpSide != LastWallJumpSide.Left)
         {
             rb.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
             lastWallJumpSide = LastWallJumpSide.Left;
         }
+
+        // same check for wall being on right side
         else if (jump && GroundCheck(Vector2.right) && lastWallJumpSide != LastWallJumpSide.Right)
         {
             rb.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
             lastWallJumpSide = LastWallJumpSide.Right;
         }
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Fruit"))
         {
+            // show the "collected animation" when picking up a fruit
             var collect = Instantiate(
                 collectPrefab,
                 collision.gameObject.transform.position,
@@ -78,11 +88,11 @@ public class Player : MonoBehaviour
         }
     }
 
-    bool GroundCheck(Vector2 vector, float distance = 0.5f) // <--
+    bool GroundCheck(Vector2 vector, float distance = 0.5f)
     {
         return Physics2D.CapsuleCast(
             bc.bounds.center, bc.bounds.size,
             0f, 0f,
-            vector, distance, groundLayer); // <--
+            vector, distance, groundLayer);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2d-platformer-with-wall-jump
+
+Based on 2D platformer tutorial from https://github.com/mhfalken/unity, with code to allow wall jumping
